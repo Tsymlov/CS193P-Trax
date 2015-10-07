b46cb77e5e143264ec252a4ded9d727e2b2706eb
Tsymlov <alexey.tsymlov@gmail.com>
2015-10-07 11:58:59 +0300

    After 14th lecture.

---
+++ Trax/MKGPX.swift
@@ -0,0 +1,31 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by Alexey Tsymlov on 10/7/15.
+//  Copyright © 2015 Alexey Tsymlov. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    
+    var title: String! { return name }
+    
+    var subtitle: String! {return info }
+    
+    var thumbnailURL: NSURL? {return getImageURLofType("thumbnail")}
+    var imageURL: NSURL? {return getImageURLofType("large")}
+    
+    private func getImageURLofType(type: String)->NSURL?{
+        for link in links {
+            if link.type == type {
+                return link.url
+            }
+        }
+        return nil
+    }
+}
